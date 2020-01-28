f7d72fc0b0b940157bb3848d5417dd3d4f302f01
youle921 <toshiki.urita@ci.cs.osakafu-u.ac.jp>
2020-01-29 01:07:04 +0900

    get mokp variants result

--- Knapsack/problem_research/calc_min_hamming.py
+++ Knapsack/problem_research/calc_min_hamming.py
@@ -6,24 +6,95 @@ Created on Wed Jan 15 02:18:29 2020
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 
-def min_hamming(data, datasets):
-    dist = np.logical_xor(data, datasets)
+def min_hamming(data, ref):
+    dist = np.logical_xor(data[:, None, :], ref[None, :, :])
 
-    return min(np.sum(dist, axis = 1))
+    return np.min(np.sum(dist, axis = 2), axis = 1)
 
-base = "result_moead_1.0/MOEAD/pops_"
-comp = "result_moead_1.4/MOEAD/pops_"
-ext = "gen.csv"
+def differ_hamning(data, ref):
+    
+#    differ[:, 0]: number of to0 bit
+#    differ[:, 1]: number of to1 bit
+    differ = np.empty([data.shape[0], 2])
+    
+    dist = np.logical_xor(data[:, None, :], ref[None, :, :])
+    idx = np.sum(dist, axis = 2).argmin(axis = 1)
+    min_dist = dist[range(0, data.shape[0]), idx, :]
+    differ[:, 0] = np.logical_and(ref[idx, :], min_dist).sum(axis = 1)
+    differ[:, 1] = np.sum(min_dist, axis = 1) - differ[:, 0]
+    
+    return differ
 
-dist_list = np.zeros([500, 1])
+dir_name = "set_pop/"
 
-for i in range(1, 501):
+methods = ["bitflip", "scaling"]
+names = ["result_sr", "_fr", "/pops_gen"]
+ext = ".csv"
 
-    d1 = np.loadtxt(comp + str(i) + ext, delimiter = ',')
-    base_data = np.loadtxt(base + str(i) + ext, delimiter = ',')
-    for n in range(200):
-        dist_list[i - 1] += min_hamming(d1[n, :], base_data)
+sr = ["0.6", "0.8", "1.2", "1.4"]
+fr = ["0.5", "0.4", "0.3", "0.2"]
 
-avr_dist = dist_list / 200
+dist_list = np.zeros([51, 3])
+dist_list[0, 0] = 0
+dist_list[1:, 0] = [i * 10 for i in range(1, 51)]
 
+base_data = np.zeros([51, 200, 500])
+base_data[0] = np.loadtxt(dir_name + "baseline" + names[2] + str(1) + ext, delimiter = ',')
+for i in range(50):
+    base_data[i + 1] = np.loadtxt(dir_name + "baseline" + names[2] + str((i + 1) * 10) + ext, delimiter = ',')
+
+for m in range(len(methods)):
+
+    for j in range(len(sr)):
+        
+        d1 = np.loadtxt(dir_name + names[0] + sr[j] + names[1] + fr[j] + '/' + methods[m] + names[2] + str(1) + ext, delimiter = ',')
+        dist_list[0, 1] = np.average(min_hamming(d1, base_data[0]))
+        dist_list[0, 2] = np.average(min_hamming(base_data[0], d1))
+        
+        for i in range(50):
+        
+            d1 = np.loadtxt(dir_name + names[0] + sr[j] + names[1] + fr[j] + '/' + methods[m] + names[2] + str((i + 1) * 10) + ext, delimiter = ',')
+    
+            dist_list[i + 1, 1] = np.average(min_hamming(d1, base_data[i + 1]))
+            dist_list[i + 1, 2] = np.average(min_hamming(base_data[i + 1], d1))
+            
+        dif = differ_hamning(d1, base_data[i + 1])
+
+        if m == 0:
+            np.savetxt(dir_name + methods[m] + "/hamming_distance_" + fr[j] + ext, dist_list, delimiter = ',')
+            np.savetxt(dir_name + methods[m] + "/differ_hamming_" + fr[j] + ext, dif, delimiter = ',')
+        else:
+            np.savetxt(dir_name + methods[m] + "/hamming_distance_" + sr[j] + ext, dist_list, delimiter = ',')
+            np.savetxt(dir_name + methods[m] + "/differ_hamming_" + sr[j] + ext, dif, delimiter = ',')
+
+
+d1 = np.loadtxt(dir_name + 'result_fr0.1' + names[2] + str(1) + ext, delimiter = ',')   
+dist_list[0, 1] = np.average(min_hamming(d1, base_data[0]))
+dist_list[0, 2] = np.average(min_hamming(base_data[0], d1))
+
+for i in range(50):
+
+    d1 = np.loadtxt(dir_name + 'result_fr0.1' + names[2] + str((1 + i)*10) + ext, delimiter = ',')   
+
+    dist_list[i + 1, 1] = np.average(min_hamming(d1, base_data[i]))
+    dist_list[i + 1, 2] = np.average(min_hamming(base_data[i], d1))
+    
+dif = differ_hamning(d1, base_data[i + 1])
+
+np.savetxt(dir_name + methods[0] + "/hamming_distance_0.1" + ext, dist_list, delimiter = ',')
+np.savetxt(dir_name + methods[0] + "/differ_hamming_0.1" + ext, dif, delimiter = ',')
+
+        
+#ref_data = np.loadtxt(dir_name + names[0] + "1.0" + names[1] + "500" + ext, delimiter = ',')
+#
+#for comp in ["0.6", "0.8", "1.2", "1.4"]:
+#    
+#    plt.clf()
+#    comp_data = np.loadtxt(dir_name + names[0] + comp + names[1] + "500" + ext, delimiter = ',')
+#    dif = differ_hamning(comp_data, ref_data)
+#    
+#    plt.scatter(dif[:, 0], dif[:, 1])
+#    
+#plt.show()
---
+++ Knapsack/problem_research/make_graph.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 15 13:56:05 2020
+
+@author: y5ule
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import subprocess
+
+dir_name = "set_pop/"
+
+methods = ["scaling", "bitflip"]
+names = ["/hamming_distance_", "/differ_hamming_"]
+ext = ".csv"
+
+sr = ["0.6", "0.8", "1.2", "1.4"]
+fr = ["0.1", "0.2", "0.3", "0.4", "0.5"]
+
+plt.close("all")
+
+for m in methods:
+    
+    plt.rcParams["font.size"] = 20
+    fig1, ax1 = plt.subplots(1, 1)
+    fig2, ax2 = plt.subplots(1, 1)
+        
+    if m == "scaling":
+        ar = sr
+
+    else:
+        ar = fr    
+
+    for s in ar:
+        plot_data = np.loadtxt(dir_name + m + names[0] + s + ext, delimiter = ',')
+        ax1.plot(plot_data[:, 0], (plot_data[:, 1] + plot_data[:, 2])/2, linewidth = 3, marker = 'o')
+        scatter_data = np.loadtxt(dir_name + m + names[1] + s + ext, delimiter = ',')
+        ax2.scatter(scatter_data[:, 0], scatter_data[:, 1], s = 36)
+    
+    ax1.set_xlim(0, 505)
+    ax1.set_xticks((0, 100, 200, 300, 400, 500))
+    ax1.set_xticklabels([0, 100, 200, 300, 400, 500])
+    ax1.set_ylim(-5, 250)
+
+    
+    ax2.set_ylim(-5, 130)
+    ax2.set_yticks((0, 25, 50, 75, 100, 125))
+    ax2.set_xlim(-5, 130)
+    ax2.set_xticks((0, 25, 50, 75, 100, 125))
+    
+        
+    if m == "scaling":
+        ax1.legend(['dist(0.6, 1.0)', 'dist(0.8, 1.0)', 'dist(1.2, 1.0)', 'dist(1.4, 1.0)'], fontsize = 15, bbox_to_anchor=(1.5, 1))
+        ax2.legend(['  = 0.6', '  = 0.8', '  = 1.2', '  = 1.4'], fontsize = 15, bbox_to_anchor=(1.5, 1))
+
+    else:  
+        ax1.legend(['dist(0.1, 0.0)', 'dist(0.2, 0.0)', 'dist(0.3, 0.0)', 'dist(0.4, 0.0)', 'dist(0.5, 0.0)'], fontsize = 15, bbox_to_anchor=(1.5, 1))
+        ax2.legend(['  = 0.1', '  = 0.2', '  = 0.3', '  = 0.4', '  = 0.5'], fontsize = 15, bbox_to_anchor= (1.5, 1))
+        
+    ax2.set_aspect('equal')
+        
+
+#    ax.legend(['dist(0.6, 1.0)', 'dist(0.8, 1.0)', 'dist(1.2, 1.0)', 'dist(1.4, 1.0)'], fontsize = 15, loc = 'upper right')
+#    plt.show()
+        
+    graph_names = [dir_name + m + '/dist', dir_name + m + '/dif']
+    fig1.savefig(graph_names[0] + '.svg', bbox_inches='tight')
+    fig2.savefig(graph_names[1] + '.svg', bbox_inches='tight')
+    
+    subprocess.call("inkscape " + graph_names[0] + ".svg -M " +  graph_names[0] + ".emf", shell = False)
+    subprocess.call("inkscape " + graph_names[1] + ".svg -M " +  graph_names[1] + ".emf", shell = False)   
