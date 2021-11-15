d5ca566078536e694cd7fa5884d4a464eb0980bc
youle921 <toshiki.urita@ci.cs.osakafu-u.ac.jp>
2021-11-15 17:46:04 +0900

    modify normalized igd results

--- python_implement/main/get_results.py
+++ python_implement/main/get_results.py
@@ -8,7 +8,7 @@ from implementation.problems.real_world_problem.metric_calculator import calcula
 ins = calculator(["normalized_IGD"])
 names = [f'../{alg}/1019' for alg in ["MO-MFEA", "MO-MFEA-II"]]
 names.extend([f'../{alg}/1017' for alg in ["EMEA", "Island_Model"]])
-# for n in names:
-#     print(n)
-#     ins.calculation(os.path.join(__file__, n))
+for n in names:
+    print(n)
+    ins.calculation(os.path.join(__file__, n))
 ins.single_calculation(os.path.join(__file__, "../NSGA-II/1017"))
